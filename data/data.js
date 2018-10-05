136b4f35297af06978d670f7e214bd2cdb06ae18
Your Name <helen-unger@hotmail.com>
2018-10-05 19:45:15 -0400

    added image descriptions

--- data/data.js
+++ data/data.js
@@ -1,12 +1,16 @@
 var jacobData = {
-    links: ['<a href="http://www.instagram.com/jiatkinson7"><i class="fab fa-instagram"></i></a>',
+        links: ['<a href="http://www.instagram.com/jiatkinson7"><i class="fab fa-instagram"></i></a>',
             '<a href="http://www.youtube.com/1jiatkinson1"><i class="fab fa-youtube"></i></a>',
             '<a href="http://www.linkedin.com/in/jacob-atkinson-906405117"><i class="fab fa-linkedin"></i></a>',
             '<a href="http://jiatkinson@xcelco.on.ca"><i class="fas fa-at"></i></a>'],
 
-    videos: ['<iframe src="https://www.youtube.com/embed/kDK-dTcg6J0?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+        videos: ['<iframe src="https://www.youtube.com/embed/kDK-dTcg6J0?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
             '<iframe src="https://www.youtube.com/embed/kDK-dTcg6J0?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
-            '<iframe src="https://www.youtube.com/embed/_J1llAPa2bo?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>']
+            '<iframe src="https://www.youtube.com/embed/_J1llAPa2bo?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'],
+
+        projectDesc: ['Conquer The Coast | Ontario’s Blue Coast - Editor',
+                      'Hessenland Country Inn Promotional Video',
+                        'On My High – Passion Project']
 };
 
 
@@ -14,5 +18,9 @@ var helenData = {
     links: ['<a href="http://www.instagram.com/img_src__"><i class="fab fa-instagram"></i></a>',
             '<a href="http://www.github.com/HelenUnger"><i class="fab fa-github"></i></a>',
             '<a href="https://www.linkedin.com/in/helen-unger/"><i class="fab fa-linkedin"></i></a>',
-            '<a href="http://helen-unger@hotmail.com"><i class="fas fa-at"></i></a>']
+            '<a href="http://helen-unger@hotmail.com"><i class="fas fa-at"></i></a>'],
+        
+        projectDesc: ['Reactr Website - Backend using Laravel and mySQL',
+                        'Personal Photography',
+                        'school 3D modelling project']
 };
