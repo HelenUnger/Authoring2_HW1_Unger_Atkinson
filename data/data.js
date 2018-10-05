f6c5603fd56b21b921cb8ffeaff516fa3b591cb5
Your Name <helen-unger@hotmail.com>
2018-10-05 18:25:04 -0400

    fixed and added content

--- data/data.js
+++ data/data.js
@@ -1,24 +1,18 @@
 var jacobData = {
-    description: "DESC",
-    links: ['<a href="www.instagram.com/jiatkinson7"><i class="fab fa-instagram"></i></a>',
-            '<a href="www.youtube.com/1jiatkinson1"><i class="fab fa-youtube"></i></a>',
-            '<a href="www.linkedin.com/in/jacob-atkinson-906405117"><i class="fab fa-linkedin"></i></a>',
-            '<a href="jiatkinson@xcelco.on.ca"><i class="fas fa-at"></i></a>'],
+    links: ['<a href="http://www.instagram.com/jiatkinson7"><i class="fab fa-instagram"></i></a>',
+            '<a href="http://www.youtube.com/1jiatkinson1"><i class="fab fa-youtube"></i></a>',
+            '<a href="http://www.linkedin.com/in/jacob-atkinson-906405117"><i class="fab fa-linkedin"></i></a>',
+            '<a href="http://jiatkinson@xcelco.on.ca"><i class="fas fa-at"></i></a>'],
 
-    images: ['jacob-image-1', 'jacob-image-2', 'jacob-image-3'],
-
-    video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/Fi7_EujNrk0?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+    videos: ['<iframe src="https://www.youtube.com/embed/kDK-dTcg6J0?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+            '<iframe src="https://www.youtube.com/embed/kDK-dTcg6J0?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+            '<iframe src="https://www.youtube.com/embed/_J1llAPa2bo?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>']
 };
 
 
 var helenData = {
-    description: "DESC",
-    links: ['<a href="www.instagram.com"><i class="fab fa-instagram"></i></a>',
-            '<a href="www.instagram.com"><i class="fab fa-github"></i></a>',
-            '<a href="www.instagram.com"><i class="fab fa-linkedin"></i></a>',
-            '<a href="www.instagram.com"><i class="fas fa-at"></i></a>'],
-
-    images: ['helen-image-1', 'helen-image-2', 'helen-image-3'],
-
-    video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/Fi7_EujNrk0?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+    links: ['<a href="http://www.instagram.com/img_src__"><i class="fab fa-instagram"></i></a>',
+            '<a href="http://www.github.com/HelenUnger"><i class="fab fa-github"></i></a>',
+            '<a href="https://www.linkedin.com/in/helen-unger/"><i class="fab fa-linkedin"></i></a>',
+            '<a href="http://helen-unger@hotmail.com"><i class="fas fa-at"></i></a>']
 };
