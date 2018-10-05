0fa857a711123169e7b97555b17e42db955c050a
Your Name <helen-unger@hotmail.com>
2018-10-05 14:15:07 -0400

    added lightbox

---
+++ data/data.js
@@ -0,0 +1,24 @@
+var jacobData = {
+    description: "DESC",
+    links: ['<a href="www.instagram.com"><i class="fab fa-instagram"></i></a>',
+            '<a href="www.instagram.com"><i class="fab fa-youtube"></i></a>',
+            '<a href="www.instagram.com"><i class="fab fa-linkedin"></i></a>',
+            '<a href="www.instagram.com"><i class="fas fa-at"></i></a>'],
+
+    images: ['jacob-image-1', 'jacob-image-2', 'jacob-image-3'],
+
+    video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/Fi7_EujNrk0?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+};
+
+
+var helenData = {
+    description: "DESC",
+    links: ['<a href="www.instagram.com"><i class="fab fa-instagram"></i></a>',
+            '<a href="www.instagram.com"><i class="fab fa-github"></i></a>',
+            '<a href="www.instagram.com"><i class="fab fa-linkedin"></i></a>',
+            '<a href="www.instagram.com"><i class="fas fa-at"></i></a>'],
+
+    images: ['helen-image-1', 'helen-image-2', 'helen-image-3'],
+
+    video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/Fi7_EujNrk0?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+};
--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@
     //VARIABLES 
     const jacobHead = document.querySelector('.personOne'),
         helenHead = document.querySelector('.personTwo'),
+        jacobHeadImg = jacobHead.querySelector('img'),
+        helenHeadImg = helenHead.querySelector('img'),
         jacobTitle = document.querySelector('#jacob-title'),
         helenTitle = document.querySelector('#helen-title'),
         jacobDesc = document.querySelector('.j-desc'),
@@ -12,34 +14,29 @@
         jacobBoxes = document.querySelectorAll('.j-box'),
         helenBoxes = document.querySelectorAll('.h-box'),
         mainJBox = document.querySelector('.jacob-boxes'),
-        mainHBox = document.querySelector('.helen-boxes');
+        mainHBox = document.querySelector('.helen-boxes'),
+        lightBox = document.querySelector('.light-box'),
+        boxes = document.querySelectorAll('.box'),
+        x = document.querySelector('.exit');
 
     //FUNCTIONS
-    // function addBoxes(e){
-    //     console.log(e);
-    //     if(e=='jacob'){
-    //         jacobBoxes.firstElementChild.classList.add('appear');
-    //         jacobBoxes.firstElementChild.classList.remove('opacity');
-    //     }else if(e=='helen'){
-    //         helenBoxes.classList.remove('hidden');
-    //     }
-    // }
-
     function openSide(e){
-        if (this == jacobHead || this == jacobTitle){
-            jacobHead.firstChild.classList.add('slide-left');
+        if (this == jacobHeadImg || this == jacobTitle){
+            jacobHead.classList.add('slide-left');
             helenHead.classList.add('opacity');
             helenTitle.classList.add('hidden');
+            jacobHead.querySelector('.social-media').classList.add('visible');
             jacobTitle.classList.add('main-title');
             jacobDesc.classList.add('show-desc');
             mainHBox.classList.add('hidden');
             jacobBoxes.forEach(function(element) {
                 element.classList.add('grow');
               });
-        }else if(this == helenHead || this == helenTitle){
-            helenHead.firstChild.classList.add('slide-right');
+        }else if(this == helenHeadImg || this == helenTitle){
+            helenHead.classList.add('slide-right');
             jacobHead.classList.add('opacity');
             jacobTitle.classList.add('hidden');
+            helenHead.querySelector('.social-media').classList.add('visible');
             helenTitle.classList.add('main-title');
             helenDesc.classList.add('show-desc');
             mainJBox.classList.add('hidden');
@@ -51,13 +48,28 @@
         }
     }
 
+    function showLightBox(){
+        let source = this.firstChild.getAttribute('src');
+        lightBox.classList.add('shown');
+        bigImage = document.createElement("IMG");
+        bigImage.setAttribute("src", source);
+        bigImage.setAttribute("class", "lightBoxImage");
+        lightBox.appendChild(bigImage);
+        console.log(source);
+    }
+
+    function closeLightBox(){
+        lightBox.classList.remove('shown');
+    }
 
 
     //EVENTLISTENERS
-    jacobHead.addEventListener('click', openSide);
-    helenHead.addEventListener('click', openSide);
+    jacobHeadImg.addEventListener('click', openSide);
+    helenHeadImg.addEventListener('click', openSide);
     jacobTitle.addEventListener('click', openSide);
     helenTitle.addEventListener('click', openSide);
+    boxes.forEach(box => box.addEventListener('click', showLightBox));
+    x.addEventListener('click', closeLightBox);
     
 
 })();
